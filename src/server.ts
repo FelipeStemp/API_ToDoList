e9b810befd229f1d5e20598ac8c46d96e07d1472
felipestemp <felipestempkowski17@gmail.com>
2024-10-28 19:38:42 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
